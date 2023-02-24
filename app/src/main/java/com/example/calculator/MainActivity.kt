081cb4810004204868ac3f98771ae3a92f99f25a
efletcher99 <ethanfletch99@gmail.com>
2023-02-24 13:36:26 -0700

    finished to assignment 2 requirements

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,10 +2,41 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    fun number(view: View)
+    {
+        if(view is Button)
+        {
+            result.append(view.text)
+        }
+    }
+    fun operation(view: View)
+    {
+        if(view is Button)
+        {
+            result.append(view.text)
+        }
+    }
+
+    fun equals(view: View) {
+
+    }
+
+    fun clear(view: View)
+    {
+        result.text = ""
+
     }
 }
